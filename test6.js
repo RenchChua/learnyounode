e98bbd8aef163755f1e8cff0d1ada4c4a0669b0b
Rencheng Chua <rench00@gmail.com>
2016-08-11 22:57:43 +0800

    finish test7 and test8

--- test6.js
+++ test6.js
@@ -4,7 +4,7 @@ var filterStr = process.argv[3];
 
 filterFunc(dir, filterStr, function (err, list) {
   if (err){
-    return console.error('There was an error:', err)}
+    return console.error('There was an error:', err);}
   list.forEach(function (file) {
     console.log(file);
   });
